3a81e89928ca38ff5a8c77a10ca764fe039b3a29
LHT082024 <LHT082024@gmail.com>
2024-12-03 11:28:21 +0100

    making a list of profiles

--- Controllers/loginController.cs
+++ Controllers/loginController.cs
@@ -2,11 +2,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IllumiantiArchive.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace IllumiantiArchive.Controllers
 {
+    [ApiController]
+    [Route("profiles")]
     public class loginController
     {
+        List<Profiles> profiles = new List<Profiles>()
+        {
+            new Profiles{}
+
+        };
 
 
     }
