7099abaaf52ef3aec87c992ff6a297d2f9131d6e
LHT082024 <LHT082024@gmail.com>
2024-12-03 12:51:12 +0100

    session storage attempt

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDistributedMemoryCache(); //trying to store the state of being logged in on the server
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
