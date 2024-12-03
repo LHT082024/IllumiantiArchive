5807fe0f5d14e41456fb4f1fe5b699761ae6d4d4
LHT082024 <LHT082024@gmail.com>
2024-12-03 10:03:49 +0100

    finished seperating the article controller class

--- Controllers/ArticleGrade1Controller.cs
+++ Controllers/ArticleGrade1Controller.cs
@@ -8,11 +8,11 @@ using Microsoft.AspNetCore.Mvc;
 namespace IllumiantiArchive.Controllers
 {
     [ApiController]
-    [Route("Controllers")]
+    [Route("Grade 1")]
     public class ArticleGrade1Controller
     {
         //the most dangoures anomalies only leaders will have acsess to these files
-        private static List<Articles> ArticlesGrade1 = new List<Articles>()
+        private static List<Articles> articlesGrade1 = new List<Articles>()
         {
             new Articles{ID = 1,
             Name = "Odin",
@@ -52,49 +52,13 @@ namespace IllumiantiArchive.Controllers
         };
 
         //5/10 dangoures Anomolies, most employees will know about these.
-        private static List<Articles> ArticlesGrade2 = new List<Articles>()
-        {
-            new Articles
-             {ID = 4,
-             Name = "Mi-go",
-             Aliases = "The Fungi",
-             Threatlvl = 4,
-             Desig = "Bug",
-             Found = "Usually on moons of planets with human inhabitants",
-             Caused = "traveling",
-             Apperal = "Generaly looks like a crustacean animal but bodies made up of eldritch matter",
-             Information = "they travel to planets to extract metals and knowledge to what end we are not sure",
-             FinalThoughts = "any sighting are to be reported directly to your superior. avoid any and all hostile actions unless it is in self defense, dont accept any offers of knowledge"
-             },
-
-             new Articles
-             {ID = 5,
-             Name = "Zann",
-             Aliases = "the Musican",
-             Threatlvl = 3,
-             Desig = "Eldritch corrupted human",
-             Found = "Usually found with one of the eldritch gods, permant resident possibly n1905",
-             Caused = "Unatrual sound",
-             Apperal = "A hooded figure with a violin",
-             Information = "The Musican is known for playing beautiful but uncanny music, has been seen several times playing for different deities",
-             FinalThoughts = "Every encounter with the man has been plesant, valuble resource in gathering information about the gods. Chatting recomended but dont let him play for you"
-             }
-
-        };
 
         //Grade 1 articles.
         [HttpGet]
         public IEnumerable<Articles> GetGrade1()
         {
-            return ArticlesGrade1;
+            return articlesGrade1;
         }
-
-        //grade 2 articles
-        public IEnumerable<Articles> GetGrade2()
-        {
-            return ArticlesGrade2;
-        }
-
 
     }
 }
--- Controllers/ArticleGrade2Controller.cs
+++ Controllers/ArticleGrade2Controller.cs
@@ -2,16 +2,53 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IllumiantiArchive.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IllumiantiArchive.Controllers
 {
     [ApiController]
-    [Route("Controllers")]
+    [Route("Grade2")]
 
+    //5/10 dangoures Anomolies, most employees will know about these.
     public class ArticleGrade2Controller
     {
 
+        private static List<Articles> articlesGrade2 = new List<Articles>()
+        {
+
+              new Articles
+             {ID = 4,
+             Name = "Mi-go",
+             Aliases = "The Fungi",
+             Threatlvl = 4,
+             Desig = "Bug",
+             Found = "Usually on moons of planets with human inhabitants",
+             Caused = "traveling",
+             Apperal = "Generaly looks like a crustacean animal but bodies made up of eldritch matter",
+             Information = "they travel to planets to extract metals and knowledge to what end we are not sure",
+             FinalThoughts = "any sighting are to be reported directly to your superior. avoid any and all hostile actions unless it is in self defense, dont accept any offers of knowledge"
+             },
+
+             new Articles
+             {ID = 5,
+             Name = "Zann",
+             Aliases = "the Musican",
+             Threatlvl = 3,
+             Desig = "Eldritch corrupted human",
+             Found = "Usually found with one of the eldritch gods, permant resident possibly n1905",
+             Caused = "Unatrual sound",
+             Apperal = "A hooded figure with a violin",
+             Information = "The Musican is known for playing beautiful but uncanny music, has been seen several times playing for different deities",
+             FinalThoughts = "Every encounter with the man has been plesant, valuble resource in gathering information about the gods. Chatting recomended but dont let him play for you"
+             }
+        };
+
+        //grade 2 articles
+        public IEnumerable<Articles> GetGrade2()
+        {
+            return articlesGrade2;
+        }
 
     }
 }
