13dfd70e5f8b556597e3429a880522d8b879a69a
LHT082024 <LHT082024@gmail.com>
2024-12-05 10:38:37 +0100

    shit works

--- obj/Debug/net8.0/IllumiantiArchive.AssemblyInfo.cs
+++ obj/Debug/net8.0/IllumiantiArchive.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IllumiantiArchive")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df2498de1160ff216a6316eccab9f80ba12feec8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e613dce315ac7c1e98ab8ef679a57827fb549091")]
 [assembly: System.Reflection.AssemblyProductAttribute("IllumiantiArchive")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IllumiantiArchive")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
